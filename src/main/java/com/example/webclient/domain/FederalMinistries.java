a618a627eb08257d1ecc52974f612df1edf44669
Alex <lxkrmr@icloud.com>
2022-06-01 13:28:03 +0200

    Replaced federal ministry display names with names to ensure matching

--- src/main/java/com/example/webclient/domain/FederalMinistries.java
+++ src/main/java/com/example/webclient/domain/FederalMinistries.java
@@ -19,37 +19,48 @@ public class FederalMinistries {
     }
 
     private static List<Department> initializeDepartments() {
-        Department auswaertigesAmt = Department.create("Auswärtiges Amt");
-        Department justiz = Department.create("Bundesministerium der Justiz",
-                                              List.of("Deutsches Patent- und Markenamt",
-                                                      "Bundesamt für Justiz"));
-        Department finanzen = Department.create("Bundesministerium der Finanzen",
-                                                List.of("Bundeszentralamt für Steuern",
-                                                        "Generalzolldirektion",
-                                                        "ITZ-Bund"));
-        Department verteidigung = Department.create("Bundesministerium der Verteidigung");
-        Department innern = Department.create("Bundesministerium des Innern",
-                                              List.of("Bundesinstitut für Bau-, Stadt- und Raumforschung (BBSR) im Bundesamt für Bauwesen und Raumordnung (BBR)",
-                                                      "Bundesausgleichsamt",
-                                                      "Bundesverwaltungsamt",
-                                                      "Statistisches Bundesamt"));
-        Department arbeitUndSoziales = Department.create("Bundesministerium für Arbeit und Soziales",
-                                                         List.of("Bundesanstalt für Arbeitsschutz und Arbeitsmedizin"));
-        Department bildungUndForschung = Department.create("Bundesministerium für Bildung und Forschung");
+        Department auswaertigesAmt = Department.create("auswaertiges-amt");
 
-        Department familieSeniorenFrauenUndJugend = Department.create("Bundesministerium für Familie, Senioren, Frauen und Jugend");
-        Department wirtschaftlicheZusammenarbeitUndEntwicklung = Department.create("Bundesministerium für wirtschaftliche Zusammenarbeit und Entwicklung");
-        Department wirtschaftUndEnergie = Department.create("Bundesministerium für Wirtschaft und Energie",
-                                                            List.of("Bundesamt für Wirtschaft und Ausfuhrkontrolle",
-                                                                    "Bundesanstalt für Materialforschung und -prüfung"));
-        Department ernaehrungUndLandwirtschaft = Department.create("Bundesministerium für Ernährung und Landwirtschaft",
-                                                                   List.of("Bundesamt für Verbraucherschutz und Lebensmittelsicherheit",
-                                                                           "Bundessortenamt",
-                                                                           "Max Rubner-Institut"));
-        Department gesundheit = Department.create("Bundesministerium für Gesundheit",
-                                                  List.of("Bundesamt für Soziale Sicherung"));
-        Department verkehrUndDigitaleInfrastruktur = Department.create("Bundesministerium für Verkehr und digitale Infrastruktur",
-                                                                       List.of("mCLOUD"));
+        Department justiz = Department.create("bmj",
+                                              List.of("dpma",
+                                                      "bundesamt-fur-justiz"));
+
+        Department finanzen = Department.create("bmf",
+                                                List.of("bzst",
+                                                        "zoll",
+                                                        "itzbund"));
+
+        Department verteidigung = Department.create("bmvg");
+
+        Department innern = Department.create("bmi",
+                                              List.of("bbsr",
+                                                      "badv",
+                                                      "bva",
+                                                      "statistisches-bundesamt"));
+
+        Department arbeitUndSoziales = Department.create("bmas",
+                                                         List.of("baua"));
+
+        Department bildungUndForschung = Department.create("bundesministerium-fur-bildung-und-forschung");
+
+        Department familieSeniorenFrauenUndJugend = Department.create("bmfsfj");
+
+        Department wirtschaftlicheZusammenarbeitUndEntwicklung = Department.create("bmz");
+
+        Department wirtschaftUndEnergie = Department.create("bmwi",
+                                                            List.of("bafa",
+                                                                    "bam"));
+
+        Department ernaehrungUndLandwirtschaft = Department.create("bmel",
+                                                                   List.of("bvl",
+                                                                           "bsa",
+                                                                           "mri"));
+
+        Department gesundheit = Department.create("bmg",
+                                                  List.of("bas"));
+
+        Department verkehrUndDigitaleInfrastruktur = Department.create("bmdv",
+                                                                       List.of("mcloud"));
 
         return List.of(auswaertigesAmt,
                        justiz,
